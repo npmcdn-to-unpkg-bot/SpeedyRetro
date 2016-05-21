fb99eb8c0a821ae7b090d8e4f0791e9603ed59d1
Lennox M <Mackolicious@googlemail.com>
2016-05-21 23:38:30 +0100

    Fixes for separation of add retro component

--- SpeedyRetro/app/component/_start/app.component.ts
+++ SpeedyRetro/app/component/_start/app.component.ts
@@ -1,59 +1,45 @@
 import {Component, OnInit, DynamicComponentLoader, ElementRef} from 'angular2/core';
-import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Router, RouteConfig, ROUTER_DIRECTIVES, RouteParams} from 'angular2/router';
 import {Subject, Observer} from 'rxjs/Rx';
 
 import {LoginComponent} from '../../component/login/login.component';
 import {BoardComponent} from '../../component/board/board.component';
-import {RetroService} from '../../hub/svc/retro.service';
+import {AddRetroComponent} from '../../component/addretro/addretro.component';
 import {Retro} from '../../hub/entities/retro';
 import {CommentService} from '../../hub/svc/comment.service';
 
 @Component({
     selector: 'my-app',
     template:'<router-outlet></router-outlet>',
-    //templateUrl: 'app/component/_start/html/app.component.html',
-    //styleUrls: ['app/component/_start/css/app.component.css'],
-    directives: [ROUTER_DIRECTIVES, LoginComponent],
-    providers: [RetroService],
+    directives: [ROUTER_DIRECTIVES, LoginComponent]
 })
-    @RouteConfig([
-        { path: '/login/', name: 'Login', component: LoginComponent },
-        { path: '/retro/:retroId', name: 'Retros', component: BoardComponent }
+@RouteConfig([
+    { path: '/login/', name: 'Route-Login', component: LoginComponent },
+    { path: '/start/', name: 'Route-Add-Retro', component: AddRetroComponent },
+    { path: '/retro/:retroId', name: 'Route-Retro-Board', component: BoardComponent }
 ])
 export class AppComponent implements OnInit {
     error: string;
     retro: Retro;
     userExists: boolean = true;
-    retroExist: boolean = true;
+    retroExist: boolean = false;
 
-    constructor(private _retroService: RetroService,
-        private _router: Router,
+    constructor(private _router: Router,
         private _dynamicComponentLoader: DynamicComponentLoader,
         private _elementRef: ElementRef) {
     }
 
     ngOnInit() {
-        //this.renderUser();
-
         if (!this.retroExist) {
-            this._retroService.add()
-                .subscribe(retro => {
-                    this._router.navigate(['Retros', { 'retroId': retro.id }]);
-                }, error => this.error = <any>error);
+            this._router.navigate(['Route-Add-Retro']);
         }
         if (!this.userExists) {
-            this._router.navigate(['Login']);
+            this._router.navigate(['Route-Login']);
         }
         else {
-            //render dashboard
+            //let retroId = this._routeParams.get('retroId');
+
+            //this._router.navigate(['Route-Retro-Board', { 'retroId': retroId }]);
         }
     }
-
-    //createRetro() {
-
-    //}
-
-    //private renderUser() {
-    //    this._dynamicComponentLoader.loadIntoLocation(LoginComponent, this._elementRef, 'login');
-    //}
 }
--- SpeedyRetro/app/component/addRetro/addRetro.component.ts
+++ SpeedyRetro/app/component/addRetro/addRetro.component.ts
@@ -1,23 +1,35 @@
 import {Component, OnInit, DynamicComponentLoader, ElementRef} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
+import {RetroService} from '../../hub/svc/retro.service';
 import {LoginComponent} from '../../component/login/login.component';
 
 @Component({
-    selector: 'my-retro',
+    selector: 'sr-addRetro',
     templateUrl: 'app/component/addRetro/html/addRetro.component.html',
     styleUrls: ['app/component/addRetro/css/addRetro.component.css'],
+    providers: [RetroService],
 })
 
 export class AddRetroComponent implements OnInit {
     userExists: boolean = false;
+    error: string;
 
     constructor(private _dynamicComponentLoader: DynamicComponentLoader,
-        private _elementRef: ElementRef) {
+        private _elementRef: ElementRef,
+        private _retroService: RetroService,
+        private _router: Router) {
     }
 
     ngOnInit() {
         this.renderUser();
+    }
+
+    createRetro() {
+        this._retroService.add()
+            .subscribe(retro => {
+                this._router.navigate(['Route-Retro-Board', { 'retroId': retro.id }]);
+            }, error => this.error = <any>error);
     }
 
     private renderUser() {
